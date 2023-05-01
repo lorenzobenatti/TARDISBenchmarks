c04e8f583fa3d4762d516c5c167bd5b4b1b381f1
lorenzobenatti <lorenzo.benatti1@gmail.com>
2023-05-01 22:07:53 +0200

    Removed evosuite from benchmarks list

--- JacoLaunch.sh
+++ JacoLaunch.sh
@@ -94,7 +94,7 @@ echo "[JACOCO LAUNCHER] Create JACOCOCLI report html for $BENCHMARK"
 java -jar "$jacoco_cli" report jacoco.exec --classfiles "$bin_classes" --sourcefiles "$classes_source_folder" --html report
 
 #MOVING THE FOLDER IN OTHER PATH
-result_path="Reports/$project/report$BENCH"
+result_path="Reports/report$BENCH"
 mv report $result_path && echo "[JACOCO LAUNCHER] Successfully moved results to: $result_path" || echo "[JACOCO LAUNCHER] Failed to move results to: $result_path"
 
 rm jacoco.exec
