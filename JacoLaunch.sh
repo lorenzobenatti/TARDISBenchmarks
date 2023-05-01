1d3df755953350e1c53613b5b13ebd873328ba41
lorenzobenatti <lorenzo.benatti1@gmail.com>
2023-05-01 20:50:05 +0200

    Updated jacoco paths and general timeout

--- JacoLaunch.sh
+++ JacoLaunch.sh
@@ -74,7 +74,7 @@ for file in $tests_path/*.class; do
   file_name="$(basename "$file" .class)"
   # Append the file name with the correct format to the tests variable taking only Tests and no Wrapper
   #tests="$tests $project.$file_name"
-  if [[ $file_name == *"$prefix"* ]]; then
+  if [[ $file_name == *"$prefix"* ]] && [[ $file_name != "Easy4TardisButHard4EvoTest" ]] && [[ $file_name != "Hard4TardisButEasy4EvoTest"]]; then
     tests="$tests $packages.$file_name"
   fi
 done
